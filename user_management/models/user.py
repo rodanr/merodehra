0b3b328ab329c0fb4cd1ae9e3eae2ef8b1d88f11
rodan0818 <rodanramdam0818@gmail.com>
2020-10-19 02:38:31 +0545

    Advertisement post and get advertisement lists by location added

--- user_management/models/user.py
+++ user_management/models/user.py
@@ -9,7 +9,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # username can be used as primary key here as every user should have unique username
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
     # Mobile numbers are only 10 digit long excluding the country codes
     mobile_number = db.Column(db.String(10), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
